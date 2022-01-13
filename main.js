78b05785d3a4a2f70a3938f41485c22cec1900ff
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-01-13 16:10:15 +0100

    inserita immagine

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let root = new Vue({
     el: '#root',
     data: {
-        title: 'Hello World'
+        title: 'Hello World',
+        imgURL: 'https://www.chimerarevo.com/wp-content/uploads/2020/04/immagini-gratis.jpg'
     }
 })
